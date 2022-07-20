c0bf00c7d52b64f574b1d77ef6d601bc734f9a42
AlejandroV2042419 <alejandro.villamil@correounivalle.edu.co>
2022-07-20 15:27:34 -0500

    actualizar login

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
-app.use(cors());
-
 /*----------- AUTENTICACIÓN ------------- */
 
 const TOKEN_KEY = "x4TvnErxRETbVcqaLl5dqMI115eNlp5y";
@@ -49,14 +47,23 @@ app.post("/usuario/login", async (req, res) => {
             { expiresIn: "2h" }
         );
 
-        let nDatos = { token, ...use.rows };
+        //Traemos la información del usuario cuando ya se confirma la autenticación
+        const info = await pool.query("SELECT nombre_usuario , nombre_rol , url_img_usuario FROM usuario INNER JOIN rol ON rol.id_rol = usuario.rol_usuario WHERE correo_usuario = $1;", [usuario]);
+        let nDatos = { token, ...info.rows[0] };
+        
         res.status(200).json(nDatos);
     } else {
         res.status(400).send("Credenciales incorrectas");
     }
 });
 
-/*----------- FIN AUTENTICACIÓN ------------- */
+
+/*----------- FIN AUTENTICACIÓN
+Nombre
+Nombre del rol
+url_img
+
+------------- */
 
 
 
